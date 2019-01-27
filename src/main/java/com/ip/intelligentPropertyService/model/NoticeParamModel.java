f6004305401368ff1387e7526a3eb64a75cbe019
Jason Chen <752404662@qq.com>
2019-01-27 22:50:06 +0800

    上拉刷新

---
+++ src/main/java/com/ip/intelligentPropertyService/model/NoticeParamModel.java
@@ -0,0 +1,22 @@
+package com.ip.intelligentPropertyService.model;
+
+public class NoticeParamModel {
+    private String menu;
+    private int index;
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
